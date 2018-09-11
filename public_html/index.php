12767aa3c8093a5f52b990690f52a9b073a8d2c0
Nicolaj Knudsen <nicokn@rm.dk>
2018-09-11 15:51:41 +0200

    play

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,16 @@
 <?php
 
 // require 'cms.php';
+// phpinfo();
+require 'vendor/kint.php';
+
 
 echo 'Hello World!';
+echo getenv('TEST_VAR__') ?? 'missing';
+d(getenv('TEST_VAR__'));
+
+
+echo '<br />';
+
+
+echo false ? '' : '1234567';
